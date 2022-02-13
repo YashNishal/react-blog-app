5906dd40f46e08bdbeb416ab1bd9c6b5c31899e2
Himanshu <zhimanshuk135@gmail.com>
2022-02-14 00:57:44 +0530

    Completed the intro TV template on Home page

---
+++ src/components/introTV/final.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+
+function Final() {
+
+    return (
+    <p>
+        <Typewriter words={['This is the best part of Animation!']}/>
+    </p>
+  )
+}
+
+export default Final
--- src/components/introTV/introTv.js
+++ src/components/introTV/introTv.js
@@ -1,54 +1,83 @@
 import React, {useState, useEffect} from 'react'
 import Style from './IntroTv.module.css'
+import { Typewriter } from 'react-simple-typewriter'
+import Final from './final'
+
+var AnimationDelay = 4000
 
 function IntroTv() {
 
-    const [currentContent, setCurrentContent] = useState([]);
-
     const totalContent = [
-        "First line",
-        "Second line",
-        "Third line",
+        "This is the First line of Intro TV",
+        "This is the Second line of Intro TV",
+        "This is the Third line of Intro TV",
     ]
-
+    const [currentContent, setCurrentContent] = useState([]);    
+    const[finalAnimation, setFinalAnimation] = useState("");    
+    const[finalProps, setFinalProps] = useState("");    
+    const[isFinalAnimation, setIsFinalAnimation] = useState(0);
+    let i = 0;
+    
+    
     const loadLine = line => {
-        let i = 0;
-        let n = line.length;
-        let lineInterval = setInterval(() => {
-            setCurrentContent( (prevContent) => [...prevContent, line])
-
-        })
+        setCurrentContent( prev => [...prev, line])
     }
-
+    
     const loadLines = () => {
-        let i = 0;
-        let n = totalContent.length;
 
-        let Linesinterval = setInterval(() => {
-            console.log(totalContent[i]);
-
+        let Linesinterval = setInterval(async() => {
+            console.log(currentContent);
+            // console.log(totalContent[i]);
             loadLine(totalContent[i]);
             i++;
-            if(i >= n)clearTimeout(Linesinterval);
-        }, 2000);
+            if(i >= totalContent.length){
+                clearInterval(Linesinterval);
+                //After loadLines, the next animation is written here
+                var timeOut = setTimeout(() => {
+                    clearTimeout(timeOut);
+                    console.log("next animation");
+                    setFinalAnimation(Final)
+                    var timeOut2 = setTimeout( () => {
+                        clearTimeout(timeOut2);
+                        setIsFinalAnimation(1);
+                        setFinalProps(<div></div>);
+                    }, AnimationDelay)
+
+                }, AnimationDelay);
+            }
+        }, AnimationDelay);
+    }
+
+    const AnimateIntroTv = async() => {
+        setCurrentContent([])
+        loadLines(); //Next effect after loadLines is called in the callback of setInterval written in loadLines
     }
 
     useEffect(() => {
-        setCurrentContent([])
-        loadLines();
+        AnimateIntroTv();
+
+        return( () => {
+
+        })
     }, [])
     
 
-
     return (
-        <div className={Style.introTv}>
+        <div className={(isFinalAnimation === 1)? Style.introTv + " " + Style.finalAnimation : Style.introTv}>
             <div className={Style.menuBar}>
                 <div className={Style.circle + " " + Style.Red} ></div>
                 <div className={Style.circle + " " + Style.Yellow}></div>
                 <div className={Style.circle + " " + Style.Green}></div>
             </div>
             <div className={Style.content}>
-                {currentContent.map( line => <p key={line}>{line}</p>)}
+                {currentContent.map( line => 
+                    <div key={line}>
+                        <Typewriter className= {Style.contentLine} words={[line]}/>
+                    </div>
+                
+                )}
+                {finalAnimation}
+                {finalProps}
             </div>
         </div>
     )
