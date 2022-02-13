7e23f3a6ed1735902165d9c26b8c4e64a0926f66
Himanshu <zhimanshuk135@gmail.com>
2022-02-14 01:13:56 +0530

    Added coments on the IntroTV component

--- src/components/introTV/final.js
+++ src/components/introTV/final.js
@@ -4,6 +4,7 @@ import { Typewriter } from 'react-simple-typewriter'
 function Final() {
 
     return (
+        //This will be the final text i.e. the text in Green
     <p>
         <Typewriter words={['This is the best part of Animation!']}/>
     </p>
